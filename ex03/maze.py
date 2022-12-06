c3227a1e5dd1018a87ad16e77d836c76d699ac23
c0a21105 <c0a2110575@edu.teu.ac.jp>
2022-12-06 14:15:10 +0900

    6問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,10 @@ def key_down(event):
     global key
     key = event.keysym
 
+def key_up(event):
+    global key
+    key = ""
+
 if __name__ == "__main__":
     root = tk.Tk()
     canvas = tk.Canvas(width=1500, height=900, bg="black")
@@ -16,5 +20,6 @@ if __name__ == "__main__":
     key = ""
 
     root.bind("<KeyPress>", key_down)
+    root.bind("<keyRelease>", key_up)
 
     root.mainloop()
