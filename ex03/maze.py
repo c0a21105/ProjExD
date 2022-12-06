36c89f71f1c50ea8ca0013963aa1900c91fc18df
c0a21105 <c0a2110575@edu.teu.ac.jp>
2022-12-06 14:10:39 +0900

    4問目まで

---
+++ ex03/maze.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+def key_down(event):
+    
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    canvas = tk.Canvas(width=1500, height=900, bg="black")
+    canvas.pack()
+
+    image = tk.PhotoImage(file="fig/2.png")
+
+    cx = 300
+    cy = 400
+    key = ""
+
+    root.mainloop()
